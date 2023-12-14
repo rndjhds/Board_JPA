5b12d52dc646bb7458106e4daf0387d646b5fb06
wodls1616 <wodls1616@gmail.com>
2023-12-14 21:52:25 +0900

    #31 - 해시태그 검색 페이지에서 본문으로 정렬할 경우 Long타입이나 CLOB타입은 정렬이 불가능하므로 데이터 타입의 사이즈를 작게 설정 Querydsl을 사용하기 JpaQueryFactory를 스프링 빈으로 등록

--- src/main/java/com/jpa/board/config/JpaConfig.java
+++ src/main/java/com/jpa/board/config/JpaConfig.java
@@ -1,10 +1,13 @@
 package com.jpa.board.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Optional;
 
 @Configuration
@@ -14,5 +17,10 @@ public class JpaConfig {
     @Bean
     public AuditorAware<String> auditorAware() {
         return () -> Optional.of("uno"); // 현재는 임시로 지정 후에 스프링 시큐리티로 인증 기능을 붙이게 될떄, 변경할 예정
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
     }
 }
